61f10c7d98b082f41991a7cf32cff701c2446c4f
Zachary Littrell <mysticcabbage@gmail.com>
2012-10-24 11:33:34 -0400

    Implemented backwards algorithm for Week 5 Problem 2 (currently very, very slow)

--- problems-seminar/fall2012/Week5Problem2.java
+++ problems-seminar/fall2012/Week5Problem2.java
@@ -1,12 +1,13 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
 import java.util.List;
 import java.util.LinkedList;
 
@@ -19,34 +20,76 @@ public class Week5Problem2
   public static class IntervalTask
       implements Callable<List<Integer>>
   {
+    private final BigInteger[] powersOfTwo;
     private final int max;
     private final int start;
-    private final int step;
+    private final int end;
     /**
+     *@param powersOfTwo  The necessary powers of two precomputed.
      *@param  max  The supremum of the superset
      *@param  start  The start of the interval
      *@param  step  The step between members of the interval
      */
-    public IntervalTask(int max, int start, int step)
+    public IntervalTask(BigInteger[] powersOfTwo, int max, int start, int end)
     {
+      this.powersOfTwo = powersOfTwo;
       this.max = max;
       this.start = start;
-      this.step = step;
+      this.end = end;
     }
     public List<Integer> call() throws Exception
     {
-      BigInteger end = BigInteger.valueOf(2).pow(max);
-      BigInteger step = BigInteger.valueOf(this.step);
-      List<Integer> longest = Collections.<Integer>emptyList();
-      for (BigInteger i = BigInteger.valueOf(start);
-	   i.compareTo(end) < 0;
-	   i = i.add(step))
+      final BigInteger MAX_SUBSET = 
+	powersOfTwo[max].subtract(BigInteger.ONE);
+      //Loop through how many bits we're going to turn off
+      for (int i = start; i < end; i++)
       {
-        List<Integer> subset = binaryNumberToSubset(max,i);
-	if (isValid(subset) && subset.size() > longest.size())
-	  longest = subset;
+	ArrayDeque<BigInteger> maskStack = new ArrayDeque<BigInteger>();
+	ArrayDeque<Integer> indices = new ArrayDeque<Integer>();
+	ArrayDeque<Integer> counts = new ArrayDeque<Integer>();
+	maskStack.push(MAX_SUBSET);
+	indices.push(0);
+	counts.push(0);
+	do
+	{
+	  BigInteger mask = maskStack.pop();
+	  int index = indices.pop();
+	  int count = counts.pop();
+          //Loop through each bit in mask
+	  for (; index < max; index++)
+	  {
+            if (mask.testBit(index))
+	    {
+              BigInteger modifiedMask = mask.xor(powersOfTwo[index]);
+	      int newCount = count+1;
+	      if (newCount == i)
+	      {
+                List<Integer> subset = 
+		  binaryNumberToSubset(max,modifiedMask);
+		if (isValid(subset))
+		  return subset;
+	      }
+              else
+	      {	
+		//Go to the next permutation
+		//without flipping off the current bit
+                maskStack.push(mask);
+	        indices.push(index+1);
+	        counts.push(count);
+
+	        //Go to the next permutation
+		//with flipping this bit off.
+	        maskStack.push(modifiedMask);
+	        indices.push(index+1);
+		counts.push(newCount);
+	      }
+	    } 
+      	  }
+	} while (!maskStack.isEmpty());
       }
-      return longest;
+      //If we find no valid subset, we simply throw an exception
+      //to signal that this thread has failed.
+      throw new Exception();
     }
   }
 
@@ -102,6 +145,18 @@ public class Week5Problem2
     return true;
   }
 
+  public static BigInteger[] powers(BigInteger n, int maxPower)
+  {
+    BigInteger[] powers = new BigInteger[maxPower+1];
+    BigInteger ithPower = BigInteger.ONE;
+    for (int i = 0; i <= maxPower; i++)
+    {
+      powers[i] = ithPower;
+      ithPower = ithPower.multiply(n);
+    }
+    return powers;
+  }
+
   /**
    * Returns the longest subset of {1,2..max} such that
    * the product of all three distinct integers in the subset
@@ -109,25 +164,45 @@ public class Week5Problem2
    */
   public static List<Integer> compute(int max)
   {
+    BigInteger[] powersOfTwo = powers(BigInteger.valueOf(2),max);
     int processors = Runtime.getRuntime().availableProcessors();
     //We'd like to use as many processors as we can to distribute
     //the task.
-    ExecutorService threads = Executors.newFixedThreadPool(processors); 
-    CompletionService<List<Integer>> pool =
-      new ExecutorCompletionService<List<Integer>>(threads);
-    for (int i = 0; i < processors; i++)
-      pool.submit(new IntervalTask(max,i,processors));
-    List<Integer> longest = Collections.<Integer>emptyList();
-    for (int i = 0; i < processors; i++)
+    ExecutorService threads = 
+      Executors.newFixedThreadPool(processors,
+	  new ThreadFactory(){
+	    public Thread newThread(Runnable r){
+	      Thread thread = 
+	        Executors.defaultThreadFactory().newThread(r);
+	      thread.setDaemon(true);
+	      return thread;
+	    }
+	  });
+    List<Future<List<Integer>>> futures =
+      new ArrayList<Future<List<Integer>>>();
+    final int step = (max+1)/processors;
+    int start = 0;
+    for (int i = 0; i < processors-1; i++)
+    {
+      int next = start + step;
+      futures.add(threads.submit(new IntervalTask(powersOfTwo,max,start,next)));
+      start = next+1;
+    }
+    futures.add(threads.submit(new IntervalTask(powersOfTwo,max,start,start+step+((max+1)%processors))));
+    List<Integer> longest = null;
+    for (Future<List<Integer>> future : futures)
     {
       try
       {
-        List<Integer> subset = pool.take().get();
-        if (subset.size() > longest.size())
-       	  longest = subset;
+	longest = future.get();
+	break;
       }
-      catch(Exception ex)
+      catch(ExecutionException ee)
       {
+      }
+      catch(InterruptedException ie)
+      {
+	ie.printStackTrace();
       }
     }
     threads.shutdown();
